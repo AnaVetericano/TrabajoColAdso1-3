97e075b89c25983bd6c0e076a6c1ecfc93dce9a9
AnaVetericano <anyta822@gmail.com>
2026-08-01 11:33:38 -0500

    feat: Creacion data class 3

---
+++ app/src/main/java/com/example/trabajocolaborativoadso1_3/UserResponse.kt
@@ -0,0 +1,9 @@
+package com.example.trabajocolaborativoadso1_3
+
+data class UserResponse(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String
+)
